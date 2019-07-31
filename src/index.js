a02c732b03118b4e0ccee7819a6700644814c024
Madi Rysbekov <hooizit@gmail.com>
2019-08-01 02:53:29 +0600

    app architecture

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header'
+import SearchPanel from './components/search-panel'
+import TodoList from './components/todo-list'
 
-const TodoList = () => {
-    const items = ["Drink Coffee", 'Build Awesome App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return <h1>My Todo List</h1>
-};
-
-const SearchPanel = () => {
-    const placeholderText = "Type to Search"
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    return <input
-        placeholder={placeholderText}
-        style={searchStyle}
-    />
-};
 
 const App = () => {
     return (
