7ed367f9e76923be80a24878b4b0808a2a4cb64a
Madi Rysbekov <hooizit@gmail.com>
2019-08-01 01:02:27 +0600

    elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello, World!</h1>;
-
-console.log(el);
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="Search"/>
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+);
 
 ReactDOM.render(el, document.getElementById('root'));
