ef5f1aa221535b465088f14f23a9aacac8c0b234
Madi Rysbekov <hooizit@gmail.com>
2019-08-06 20:36:29 +0600

    data list

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import TodoListItem from './todo-list-item'
-const TodoList = () => {
+const TodoList = ({ data }) => {
+
+    const list = data.map((el) => {
+        return (
+            <li>
+                <TodoListItem
+                    label={el.label}
+                    important={el.important}
+                    />
+            </li>
+        );
+    });
+
     return (
         <ul>
-            <li><TodoListItem  label="Drink Coffee" /></li>
-            <li><TodoListItem  label="Build React App" important /></li>
+            {list}
         </ul>
     );
 };
